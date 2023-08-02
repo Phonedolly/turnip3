69da0781793930aff8bbb5f6f98f506e294ab86b
Phonedolly <Phonedolly@users.noreply.github.com>
2023-08-03 02:09:03 +0900

    my mistake -_-

--- app/api/writer/uploadImage/route.ts
+++ app/api/writer/uploadImage/route.ts
@@ -9,7 +9,10 @@ import fileToArrayBuffer from "file2arraybuffer";
 import path from "path";
 import { subClass } from "gm";
 import getImagesSizes from "@/lib/getImageSizes";
-const im = subClass({ imageMagick: "7+" });
+// const im = subClass({ imageMagick: true });
+import gm from "gm";
+// import sharp from "sharp";
+import Jimp from "jimp";
 
 const checkFileDuplicated = (s3: S3Client, epoch: number, fileName: string) => {
   return s3
@@ -27,6 +30,19 @@ const checkFileDuplicated = (s3: S3Client, epoch: number, fileName: string) => {
     });
 };
 
+// function getNormalSize({
+//   width,
+//   height,
+//   orientation,
+// }: {
+//   width: number | undefined;
+//   height: number | undefined;
+// }) {
+//   return (orientation || 0) >= 5
+//     ? { width: height, height: width }
+//     : { width, height };
+// }
+
 export async function POST(request: Request) {
   const s3 = initS3Client();
   const formData = await request.formData();
@@ -41,23 +57,38 @@ export async function POST(request: Request) {
     fileName = name + "_" + Date.now() + ext; // already ext has '.'
   }
 
-  const imageSize = (await new Promise((resolve, reject) => {
-    im(fileAsArrayBuffer).size((err, value) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(value);
-    });
-  })
-    .then((res) => res)
-    .catch((errReason) => {
-      console.error("failed to get size of image!");
-      console.error(errReason);
-      return NextResponse.json({ success: false, errReason }, { status: 500 });
-    })) as { height: number; width: number };
+  // const imageSize = (await new Promise((resolve, reject) => {
+  //   gm(fileAsArrayBuffer).size((err, value) => {
+  //     if (err) {
+  //       return reject(err);
+  //     }
+  //     return resolve(value);
+  //   });
+  // })
+  //   .then((res) => res)
+  //   .catch((errReason) => {
+  //     console.error("failed to get size of image!");
+  //     console.error(errReason);
+  //     return NextResponse.json({ success: false, errReason }, { status: 500 });
+  //   })) as { height: number; width: number };
+  // const image = await sharp(fileAsArrayBuffer);
+  // const metadata = await image.metadata();
+  // const imageSize = { height: metadata.height, width: metadata.width };
 
-  if (imageSize instanceof NextResponse) {
-    return imageSize;
+  // if (imageSize instanceof NextResponse) {
+  //   return imageSize;
+  // }
+  const imageSize = await Jimp.read(fileAsArrayBuffer).then((image) => {
+    const height = image.getHeight();
+    const width = image.getWidth();
+    return { height, width };
+  });
+  
+  if (!imageSize.height || !imageSize.width) {
+    return NextResponse.json(
+      { success: false, reason: "Sharp has failed to identify image size." },
+      { status: 500 },
+    );
   }
 
   let imageSizes = (await getImagesSizes(s3, epoch)) as IImageSizes;
