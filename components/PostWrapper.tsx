baaa2622ce260df0a10603e0a27c95d56175d6ad
Phonedolly <Phonedolly@users.noreply.github.com>
2023-08-03 11:03:15 +0900

    fix when first updateDate is undefined

--- components/PostWrapper.tsx
+++ components/PostWrapper.tsx
@@ -15,19 +15,21 @@ const PostWrapper = (props: {
       new Date(
         props.frontmatter.updateTime[props.frontmatter.updateTime.length - 1],
       )) ||
-    new Date();
+    null;
   return (
     <div className="flex w-full flex-col gap-y-4 py-6 md:text-lg">
       <div className="flex w-full flex-col gap-y-4 text-center">
         <h1 className="text-center font-outfit text-4xl font-bold">
           {props.frontmatter?.title}
         </h1>
-        <h2 className="font-outfit text-xl font-bold">
-          {/* {`${date.getFullYear()}-${
+        {date ? (
+          <h2 className="font-outfit text-xl font-bold">
+            {/* {`${date.getFullYear()}-${
             date.getMonth() + 1
           }-${date.getD()}-${date.getHours()}-${date.getMinutes()}`} */}
-          {`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
-        </h2>
+            {`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
+          </h2>
+        ) : null}
       </div>
       <div className="flex w-full flex-col gap-y-2 sm:px-4">
         <Component components={componentsGenerator(props.imageSizes)} />
