c67d5561e4fabc97e7282310c85a24715715ff58
Phonedolly <Phonedolly@users.noreply.github.com>
2023-08-05 14:07:11 +0900

    support first publish time and last edit time

--- components/PostWrapper.tsx
+++ components/PostWrapper.tsx
@@ -10,26 +10,49 @@ const PostWrapper = (props: {
   };
 }) => {
   const Component = useMemo(() => getMDXComponent(props.code), [props.code]);
-  const date =
+  const firstPublished =
     (props.frontmatter.updateTime &&
+      new Date(props.frontmatter.updateTime[0])) ||
+    null;
+
+  const lastEdited =
+    (props.frontmatter.updateTime &&
+      props.frontmatter.updateTime.length > 1 &&
       new Date(
         props.frontmatter.updateTime[props.frontmatter.updateTime.length - 1],
       )) ||
     null;
+
   return (
     <div className="flex w-full flex-col gap-y-4 py-6 md:text-lg">
       <div className="flex w-full flex-col gap-y-4 text-center">
         <h1 className="text-center font-outfit text-4xl font-bold">
           {props.frontmatter?.title}
         </h1>
-        {date ? (
-          <h2 className="font-outfit text-xl font-bold">
-            {/* {`${date.getFullYear()}-${
+        <div className="flex flex-col items-center">
+          {firstPublished && (
+            <div className="flex flex-row gap-x-2.5">
+              <h2 className="font-outfit">First Published</h2>
+              <h2 className="font-outfit text-xl font-bold">
+                {/* {`${date.getFullYear()}-${
             date.getMonth() + 1
           }-${date.getD()}-${date.getHours()}-${date.getMinutes()}`} */}
-            {`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
-          </h2>
-        ) : null}
+                {`${firstPublished.toLocaleDateString()} ${firstPublished.toLocaleTimeString()}`}
+              </h2>
+            </div>
+          )}
+          {lastEdited ? (
+            <div className="flex flex-row gap-x-2.5">
+              <h2 className="font-outfit">Last Edited</h2>
+              <h2 className="font-outfit text-xl font-bold">
+                {/* {`${date.getFullYear()}-${
+            date.getMonth() + 1
+          }-${date.getD()}-${date.getHours()}-${date.getMinutes()}`} */}
+                {`${lastEdited.toLocaleDateString()} ${lastEdited.toLocaleTimeString()}`}
+              </h2>
+            </div>
+          ) : null}
+        </div>
       </div>
       <div className="flex w-full flex-col gap-y-2 sm:px-4">
         <Component components={componentsGenerator(props.imageSizes)} />
