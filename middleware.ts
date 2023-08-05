c6aeb770e6415985c624cbe46b73386e73281aec
Phonedolly <Phonedolly@users.noreply.github.com>
2023-08-05 09:52:51 +0900

    update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/writer", "/writer/new", "/deploy"] };
+export const config = { matcher: ["/writer/:newOrEpoch", "/deploy"] };
 // export const config = {};
