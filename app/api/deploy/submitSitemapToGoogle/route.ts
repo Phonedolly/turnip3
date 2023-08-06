d85cea2c4667f604276bec4818e77897fa3387cc
Phonedolly <Phonedolly@users.noreply.github.com>
2023-08-07 00:51:28 +0900

    move sitemap submit fetch to server

---
+++ app/api/deploy/submitSitemapToGoogle/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const result = fetch(
+    `https://www.google.com/ping?sitemap=${
+      process.env.NEXT_PUBLIC_APP_URL as string
+    }/sitemap.xml`,
+    { cache: "no-store" },
+  )
+    .then(async (res) => {
+      const data = await res.json();
+      return NextResponse.json({ ...data }, { status: 200 });
+    })
+    .catch(() => NextResponse.json({}, { status: 500 }));
+
+  return result;
+}
--- app/deploy/page.tsx
+++ app/deploy/page.tsx
@@ -28,6 +28,100 @@ const Deploy = () => {
     if (!isCompleteDeploying) return;
   }, [isCompleteDeploying, buildEvents]);
 
+  const handleDeploy = async () => {
+    if (!env["VERCEL_DEPLOY_HOOK"]) {
+      alert("Vercel Git Hook is Unavailable!");
+      return;
+    }
+    /* Trigger build using hook */
+    const now = Date.now(); // save current time
+    await fetch(env["VERCEL_DEPLOY_HOOK"], { method: "POST" })
+      .then(() => {
+        alert("Deploy Request Submited!");
+        // setIsCompleteDeploying(true);
+      })
+      .catch((errReason) => {
+        console.error(errReason);
+        alert("Failed to Submit Request.");
+        router.replace("/");
+      });
+    console.log("trigger build hook");
+
+    /* get deployment id */
+    let deploymentId;
+    /* until get current deployment */
+    const getDeploymentId = async () =>
+      new Promise<void>(async (resolve) => {
+        const { id, created } = await fetch(`/api/deploy/getDeploymentId`).then(
+          async (res) => await res.json(),
+        );
+        console.log(id);
+        console.log(`created, now=====`);
+        console.log(`${created}, ${now}`);
+        if (created > now) {
+          console.log("created > now");
+
+          deploymentId = id;
+          resolve();
+        } else {
+          console.log("created <= now");
+          setTimeout(async () => {
+            resolve(await getDeploymentId());
+          }, 1000);
+        }
+      });
+
+    await getDeploymentId();
+
+    /* request deployment events until finish deployment */
+    const getEventList = async () =>
+      new Promise<void>(async (resolve) => {
+        const eventList = await fetch(
+          `/api/deploy/events?id=${deploymentId}`,
+        ).then(async (res) => await res.json());
+
+        console.log(eventList);
+        setBuildEvents(eventList);
+        if (
+          eventList[eventList.length - 1].payload.text.startsWith(
+            "Build cache uploaded:",
+          )
+        ) {
+          setIsCompleteDeploying(true);
+          await fetch(`/api/deploy/submitSitemapToGoogle`, {
+            cache: "no-store",
+          })
+            .then(() => {
+              setBuildEvents((prev) =>
+                prev.concat({
+                  type: "stdout",
+                  payload: {
+                    date: Date.now(),
+                    text: "Upload Sitemap to Google.",
+                  },
+                }),
+              );
+              resolve();
+            })
+            .catch(() => {
+              setBuildEvents((prev) =>
+                prev.concat({
+                  type: "stderr",
+                  payload: {
+                    date: Date.now(),
+                    text: "Failed to Upload Sitemap to Google. Check Sitemap is submitted on Google Search Console.",
+                  },
+                }),
+              );
+              resolve();
+            });
+        } else {
+          setTimeout(async () => resolve(await getEventList()), 1000);
+        }
+      });
+    await getEventList();
+  };
+
   return (
     <div className="flex h-full w-11/12 flex-col items-center justify-center rounded-2xl bg-neutral-100/90 px-3">
       <div className="flex h-full w-full flex-col items-center justify-center gap-y-8 p-4">
@@ -83,111 +177,17 @@ const Deploy = () => {
         </table>
         <button
           className="rounded-full bg-neutral-800 p-10 text-5xl font-extrabold text-white shadow-[0px_8px_32px_rgba(0,0,0,0.5)] transition duration-[400ms] ease-in-out hover:rotate-6 hover:scale-110 hover:shadow-[0px_12px_48px_rgba(0,0,0,0.5)]"
-          onClick={async () => {
-            if (!env["VERCEL_DEPLOY_HOOK"]) {
-              alert("Vercel Git Hook is Unavailable!");
-              return;
-            }
-            /* Trigger build using hook */
-            const now = Date.now(); // save current time
-            await fetch(env["VERCEL_DEPLOY_HOOK"], { method: "POST" })
-              .then(() => {
-                alert("Deploy Request Submited!");
-                // setIsCompleteDeploying(true);
-              })
-              .catch((errReason) => {
-                console.error(errReason);
-                alert("Failed to Submit Request.");
-                router.replace("/");
-              });
-            console.log("trigger build hook");
-
-            /* get deployment id */
-            let deploymentId;
-            /* until get current deployment */
-            const getDeploymentId = async () =>
-              new Promise<void>(async (resolve) => {
-                const { id, created } = await fetch(
-                  `/api/deploy/getDeploymentId`,
-                ).then(async (res) => await res.json());
-                console.log(id);
-                console.log(`created, now=====`);
-                console.log(`${created}, ${now}`);
-                if (created > now) {
-                  console.log("created > now");
-
-                  deploymentId = id;
-                  resolve();
-                } else {
-                  console.log("created <= now");
-                  setTimeout(async () => {
-                    resolve(await getDeploymentId());
-                  }, 1000);
-                }
-              });
-
-            await getDeploymentId();
-
-            /* request deployment events until finish deployment */
-            const getEventList = async () =>
-              new Promise<void>(async (resolve) => {
-                const eventList = await fetch(
-                  `/api/deploy/events?id=${deploymentId}`,
-                ).then(async (res) => await res.json());
-
-                console.log(eventList);
-                setBuildEvents(eventList);
-                if (
-                  eventList[eventList.length - 1].payload.text.startsWith(
-                    "Build cache uploaded:",
-                  )
-                ) {
-                  setIsCompleteDeploying(true);
-                  await fetch(
-                    `https://www.google.com/ping?sitemap=${
-                      process.env.NEXT_PUBLIC_APP_URL as string
-                    }/sitemap.xml`,
-                  )
-                    .then(() => {
-                      setBuildEvents((prev) =>
-                        prev.concat({
-                          type: "stdout",
-                          payload: {
-                            date: Date.now(),
-                            text: "Upload Sitemap to Google.",
-                          },
-                        }),
-                      );
-                      resolve();
-                    })
-                    .catch(() => {
-                      setBuildEvents((prev) =>
-                        prev.concat({
-                          type: "stderr",
-                          payload: {
-                            date: Date.now(),
-                            text: "Failed to Upload Sitemap to Google. Check Sitemap is submitted on Google Search Console.",
-                          },
-                        }),
-                      );
-                      resolve();
-                    });
-                } else {
-                  setTimeout(async () => resolve(await getEventList()), 1000);
-                }
-              });
-            await getEventList();
-          }}
+          onClick={handleDeploy}
         >
           Deploy
         </button>
       </div>
-      <div className="flex h-auto w-full flex-col ">
+      <div className="flex h-auto w-full flex-col py-4">
         {buildEvents &&
           buildEvents.map((buildEvent) => {
             return (
               <div
-                className={`flex w-full flex-row text-sm ${
+                className={`flex w-full flex-row gap-x-4 p-1.5 font-mono text-sm first:rounded-t-md last:rounded-b-md ${
                   buildEvent.type === "stderr" ? `bg-red-400` : `bg-white`
                 }`}
                 key={uuidv4()}
