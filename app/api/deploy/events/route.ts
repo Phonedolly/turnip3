6c4b684b8e30ce6959c3f9ddacf2acec0c7909a4
Phonedolly <Phonedolly@users.noreply.github.com>
2023-08-06 12:59:49 +0900

    fix typo

--- app/api/deploy/events/route.ts
+++ app/api/deploy/events/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const url = new URL(request.url);
-  const id = Number(url.searchParams.get("id"));
+  const id = url.searchParams.get("id");
   const result = await fetch(
     `https://api.vercel.com/v2/deployments/${id}}/events`,
     {
