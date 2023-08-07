6a3be1f27a26ee90febdc5785c79fe576df59730
Phonedolly <Phonedolly@users.noreply.github.com>
2023-08-08 02:12:55 +0900

    adjust title (3)

--- components/PostWrapper.tsx
+++ components/PostWrapper.tsx
@@ -26,7 +26,7 @@ const PostWrapper = (props: {
   return (
     <div className="flex w-full flex-col gap-y-4 py-6 md:text-lg">
       <div className="flex w-full flex-col gap-y-4 text-center lg:gap-y-8 xl:gap-y-12">
-        <h1 className="text-center font-outfit text-4xl leading-relaxed font-bold lg:text-5xl xl:text-6xl">
+        <h1 className="text-center font-outfit text-4xl leading-normal font-bold lg:text-5xl xl:text-6xl">
           {props.frontmatter?.title}
         </h1>
         <div className="flex flex-col items-center gap-y-1">
